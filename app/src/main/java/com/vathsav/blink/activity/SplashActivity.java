23c013b9edbc48172375d2305b0ccc8a9e706613
vathsav <vathsav@live.in>
2016-09-16 11:33:16 +0530

    First commit.

---
+++ app/src/main/java/com/vathsav/blink/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.vathsav.blink.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vathsav.blink.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
